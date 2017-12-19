8ce62bd63cdf96862bda8b9d1b02b94b25f2d6b1
carmenchapa <cmchapa28@gmail.com>
2017-12-19 12:54:33 +0000

    showing and hidding products description

--- src/components/Products.js
+++ src/components/Products.js
@@ -24,56 +24,23 @@ class Products extends Component {
 
 	constructor(props) {
 	    super(props);
-	    // this.state = { isVisible: false }
-	    // this.state = {x:  props}
-	    // .map((products, i) => {return {id: i, isVisible: false}})
-    	this.toggleClass = this.toggleClass.bind(this)
     	this.handleChange = this.handleChange.bind(this)	
 	}
 
 	componentWillReceiveProps(nextProps){
 		this.setState((prevState, props) => {
-			// return ({props})
 		  return props.products.map((products, i) => {return {id: i, isVisible: false}})
-		  // return {index: { id: 1, isVisible: false}, index: { id: 1, isVisible: false}, index: { id: 1, isVisible: false}}
-
 		}); 
 	}
 
-	onButtonClicked (number) {
-	    console.log(`Button ${number} was clicked`)
-	    console.log(this)
-	  }
+	handleChange(index) {
 
- 	toggleClass = (e) => {
-		console.log('aaa')
-		console.log(this)
-		console.log(this.state)
-		this.setState({
-	      isVisible: !this.state.isVisible
-	    })
-	    // this.props.openPanel(this.props.buttonNumber)
-	}
-
-	handleChange(id, isVisible, index) {
-		console.log(this.props[index])
-		console.log(this.props.products[index])
-		console.log(this.props.products)
-		console.log(id)
 		console.log(index)
 		console.log(this.state)
 		console.log(this.state[index])
-		console.log(this.state[index].isVisible)
-		// if(this.state.name.includes('test')){
-		// this.setState(...)	
-		// } else {
-		// 	this.setState?////
-		// }
+
         this.setState((state, props) => {
-        	// console.log(index)
-        	// let index = {id: index, isVisible: true}
-        	// return {...state, index}
-			return props.products.map((products, i) => {return {id: i, isVisible: i===index && this.state[index].isVisible === false ? true : false}})
+			return props.products.map((products, i) => {return {id: i, isVisible: (i===index && !this.state[index].isVisible) ? true : (i===index && this.state[index].isVisible) ? false : this.state[i].isVisible}})
        });
     }
 
@@ -82,27 +49,26 @@ class Products extends Component {
 			<ul>
 				{this.props.products.map((products, i) =>
 				
-				  <li key={products.id} id={i} style={this.state[i].isVisible ? bolder : null} onClick={(e) => this.handleChange(products.id, true, i)} >{products.title} 
-				  <Description key={products.id} id={products.id} ref={(i) => {this.i = i}} name={i} products={products}  buttonNumber={i} openPanel={this.onButtonClicked} style={this.state[i].isVisible ? nonHidden : hidden} />
+				  <li key={products.id} style={this.state[i].isVisible ? bolder : null} onClick={(e) => this.handleChange(i)} >{products.title} 
+				  <Description key={products.id} products={products} style={this.state[i].isVisible ? nonHidden : hidden} />
 				  </li>
 				)}
-				
 			</ul>
 			)
 	}
 }
 
+const Description = ({...props}) => <p style={props.style} >{props.products.description}</p>
+
+// class Description extends Component {
+// 	render(){
+// 		return(
+// 			<p style={this.props.style} >{this.props.products.description}</p>
+// 		)
+// 	}
+// }
 
 
-class Description extends Component {
-	render(){
-		return(
-			<p ref={(i) => {this.i = i}} style={this.props.style} >{this.props.products.description}</p>
-		)
-	}
-}
-
-/* <p onClick={(e) => this.onClick(e)} style={this.state.isButtonActive ? this.hidden : this.nonHidden}>{this.props.products.description}</p> */
 
 
 export default Products
