adc16b721f9ec83290e3b6ae31fbed5229808450
carmenchapa <cmchapa28@gmail.com>
2018-01-03 12:14:00 +0000

    created recived search matching a string

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,38 +3,19 @@ export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 
 export const REQUEST_PRODUCTS = 'REQUEST_PRODUCTS'
 export const RECEIVE_PRODUCTS = 'RECEIVE_PRODUCTS'
-// export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
 
 export const RECEIVE_CATEGORY = 'RECEIVE_CATEGORY'
 
 export const SELECT_CATEGORY = 'SELECT_CATEGORY'
-// export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-
-// export const INVALIDATE_CATEGORY = 'INVALIDATE_CATEGORY'
-
-// export const selectSubreddit = subreddit => ({
-//   type: SELECT_SUBREDDIT,
-//   subreddit
-// })
 
 export const selectCategory = category => ({
   type: SELECT_CATEGORY,
   category
 })
 
-// export const invalidateSubreddit = subreddit => ({
-//   type: INVALIDATE_SUBREDDIT,
-//   subreddit
-// })
-
-// export const invalidateCategory = category => ({
-//   type: INVALIDATE_CATEGORY,
-//   category
-// })
-
 export const requestPosts = subreddit => ({
   type: REQUEST_POSTS,
-  subreddit
+  // subreddit
 })
 
 export const receivePosts = (json) => ({
@@ -42,18 +23,11 @@ export const receivePosts = (json) => ({
   // subreddit,
   // posts: json.data.children.map(child => child.data),
   posts: json.data.filter(child => child.hidden === false),
-
 })
 
 export const requestProducts = subreddit => ({
   type: REQUEST_PRODUCTS,
-  subreddit
-})
-
-
-export const receiveProducts = (subreddit, json, category) => ({
-  type: RECEIVE_PRODUCTS,
-  products: json.data.filter(child => child.categories.some(kid => kid.title===category))
+  // subreddit
 })
 
 export const receiveCategory = (subreddit, json) => {
@@ -64,21 +38,27 @@ export const receiveCategory = (subreddit, json) => {
 	}  
 }
 
+export const receiveProducts = (subreddit, json, category) => ({
+  type: RECEIVE_PRODUCTS,
+  products: json.data.filter(child => child.categories.some(kid => kid.title===category))
+})
+
+export const receiveSearch = (subreddit, json, category) => ({
+  type: RECEIVE_PRODUCTS,
+  products: json.data.filter(child => child.categories.some(kid => kid.title===category) && child.title.match("Valle"))
+})
+
 
 export const fetchPosts = subreddit => dispatch => {
   dispatch(requestPosts())
   return fetch(`https://api.gousto.co.uk/products/v2.0/categories`)
-
     .then(response => response.json())
     .then(json => dispatch(receivePosts( json)))
 }
 
-
 export const fetchProducts = subreddit => dispatch => {
   dispatch(requestProducts(subreddit))
-  // return fetch(`https://www.reddit.com/r/${category}.json`)
-  return fetch(`https://api.gousto.co.uk/products/v2.0/products?includes[]=categories&includes[]=attributes&sort=position&image_sizes[]=365&image_sizes[]=400&period_id=120`)
-
+  return fetch(`https://api.gousto.co.uk/products/v2.0/products?includes[]=categories&includes[]=attributes&sort`)
     .then(response => response.json())
     .then(json => dispatch(receiveCategory(subreddit, json)))
 }
